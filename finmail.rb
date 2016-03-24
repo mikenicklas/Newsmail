71b85e3ee881c194e712e66bd51130766e81ef0d
Mike Nicklas <mike.nicklas@gmail.com>
2016-03-24 11:32:27 -0400

    Add SeekingAlpha and CNNMoney as sources

--- finmail.rb
+++ finmail.rb
@@ -13,7 +13,9 @@ module Finmail
     def initialize
       @email = 'mike.nicklas@gmail.com'
       @sources = [{title: 'U.S. Business', feed:'http://www.wsj.com/xml/rss/3_7014.xml'}, 
-                  {title: 'Markets News', feed: 'http://www.wsj.com/xml/rss/3_7031.xml'}]
+                  {title: 'Markets News', feed: 'http://www.wsj.com/xml/rss/3_7031.xml'},
+                  {title: 'CNN Markets', feed: 'http://rss.cnn.com/rss/money_markets.rss'},
+                  {title: 'Seeking Alpha', feed: 'http://seekingalpha.com/tag/wall-st-breakfast.xml'}]
     end
     
     def get_news
