ab4efa548fb2054709ba25629b55f1fff46a1113
Mike Nicklas <mike.nicklas@gmail.com>
2016-03-26 12:16:39 -0400

    Basic spec to ensure articles are passed and iterated properly

--- spec/models/mailer/builder_spec.rb
+++ spec/models/mailer/builder_spec.rb
@@ -4,9 +4,19 @@ describe Finmail::Mailer::Builder do
   
   subject { Finmail::Mailer::Builder }
   
-  it 'should render slim template' do
+  it 'should render slim template with content attrs' do
     template = subject.render(articles: [], content: {title: 'Test'})
     expect(template.include?('Test')).to eql true
+  end
+  
+  it 'should render template with articles' do
+    articles, content = [], {title: 'Your Articles'}
+    ['Article 1', 'Article 2'].each do |article|
+      opts = {link: "http://#{article}", description: "#{article * 10}", title: article}
+      articles << Finmail::Article.new('The Times', opts)
+    end
+    template = subject.render(articles: articles, content: content)
+    expect(template.split('Read More').size).to eql 3
   end
   
 end
